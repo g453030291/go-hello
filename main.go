8f2cbc54b667b351b7bd712067fd02a8bb6c0a9b
gemushen <453030291@qq.com>
2022-06-13 22:19:31 +0800

    go的项目不推荐包含src目录

--- main.go
+++ main.go
@@ -1,16 +1,17 @@
-package main
+package main222
 
-import (
-	"fmt"
-	"go-hello/src/jsondemo"
-)
-
-func main() {
-	//filedemo.ReadFile()
-	//filedemo.ReadFile2()
-	//filedemo.WriteFile()
-	//httpdemo.GetRequest()
-	//httpdemo.PostRequest()
-	fmt.Println(jsondemo.JsonFromString())
-	fmt.Println(jsondemo.StringFromJson(jsondemo.JsonFromString()))
-}
+//
+//import (
+//	"fmt"
+//	"go-hello/src/jsondemo"
+//)
+//
+//func main() {
+//	//filedemo.ReadFile()
+//	//filedemo.ReadFile2()
+//	//filedemo.WriteFile()
+//	//httpdemo.GetRequest()
+//	//httpdemo.PostRequest()
+//	fmt.Println(jsondemo.JsonFromString())
+//	fmt.Println(jsondemo.StringFromJson(jsondemo.JsonFromString()))
+//}
